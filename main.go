1c4439d9d948bdeb9dacb78966941345dfe6806c
kabir <kabirslack@gmail.com>
2026-08-30 22:35:21 +0600

    feat: add gin sever

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/uuid", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"uuid": uuid.New().String(),
+		})
+	})
+
+	r.Run(":8080") // listens and serves on 0.0.0.0:8080
 }
